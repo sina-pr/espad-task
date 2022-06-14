38649fb3c72ef13c0a3e18cb1c79b8fab319bb54
Sina Sedighi <dev.sina2000@gmail.com>
2022-06-14 19:59:53 +0430

    redirect user to customers page

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,10 +1,12 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import routes from './list';
+import { CUSTOMERS } from '../configs/constantUrl';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/' element={<Navigate to={CUSTOMERS} />} />
         {routes.map((route, index) => (
           <Route path={route.path} key={index} element={<route.component />} />
         ))}
